ecf80deaa85fb07f6304d44302204229135e7b33
Giacomo Melacini <gmelacini@unibz.it>
2021-01-05 17:54:38 +0100

    Added game environment and imported it in move

--- move.py
+++ move.py
@@ -1,10 +1,17 @@
 from flask import (Blueprint, request)
+import numpy as np
+
+from watten.games.HandWattenGame import HandWattenGame
+from watten.models.HandWattenNNet import HandWattenNNet
 
 bp = Blueprint('move', __name__, url_prefix='/move')
+
+generations = ["watten/models/model_updated_3.h5", "watten/models/model_updated_4.h5",
+               "watten/models/model_updated_6.h5", "watten/models/model_updated_21.h5"]
 
 
 @bp.route('/', methods=['POST', 'GET'])
 def get_move():
     if request.method == 'GET':
         return "Test funziona!"
-    
+

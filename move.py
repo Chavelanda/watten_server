6d42b79c92fecbfe4ac259d656fbc4dfbb80dbf1
Giacomo Melacini <gmelacini@unibz.it>
2021-01-05 18:10:51 +0100

    Added model creation

--- move.py
+++ move.py
@@ -9,6 +9,15 @@ bp = Blueprint('move', __name__, url_prefix='/move')
 generations = ["watten/models/model_updated_3.h5", "watten/models/model_updated_4.h5",
                "watten/models/model_updated_6.h5", "watten/models/model_updated_21.h5"]
 
+models = []
+
+game = HandWattenGame()
+
+x, y = game.get_observation_size()
+
+for path in generations:
+    models.append(HandWattenNNet(x, y, 1, game.get_action_size(), path))
+
 
 @bp.route('/', methods=['POST', 'GET'])
 def get_move():

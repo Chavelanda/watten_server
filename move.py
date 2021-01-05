f25c9e248df06f5cc90f6d8a6f045ad69d871cf0
Giacomo Melacini <gmelacini@unibz.it>
2021-01-05 18:39:38 +0100

    Handling post request

--- move.py
+++ move.py
@@ -1,4 +1,4 @@
-from flask import (Blueprint, request)
+from flask import (Blueprint, request, jsonify)
 import numpy as np
 
 from watten.games.HandWattenGame import HandWattenGame
@@ -23,4 +23,29 @@ for path in generations:
 def get_move():
     if request.method == 'GET':
         return "Test funziona!"
+    if request.method == 'POST':
+        # Take data from json
+        json = request.json
 
+        gen = json["generation"]
+
+        game.trueboard.init_world_to_state(-1, json["distributing"], json["hand_a"], json["hand_b"],
+                                           json["played_cards"], json["current_score_a"], json["current_score_b"],
+                                           json["current_prize"], json["is_last_move_raise"],
+                                           json["is_last_move_accepted_raise"], json["is_last_hand_raise_valid"],
+                                           json["first_card"], json["last_card"], json["rank"], json["suit"])
+
+        # Ask to the indicated model to make a prediction
+        pi, _ = models[gen].predict(game, game.get_cur_player())
+
+        # Filter the prediction with the valid actions
+        valid_moves = game.get_valid_moves(game.get_cur_player())
+
+        pi = pi*valid_moves
+
+        # Return the best possible action
+        move = np.argmax(pi)
+
+        answer = {"move": int(move)}
+        
+        return jsonify(answer)

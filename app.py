cc3ad43a2da5520a2a306ff42be0bd862e830e15
Giacomo Melacini <gmelacini@unibz.it>
2021-01-06 17:35:13 +0100

    Added config file and db instance

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from move import bp
 
 server = Flask(__name__)
 
+server.config.from_object(os.environ['APP_SETTINGS'])
+server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(server)
+
 server.register_blueprint(bp)
+
 
 @server.route('/')
 def hello_world():

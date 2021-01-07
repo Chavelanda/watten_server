a46b3208cde8f3a61ce21eb73fe35cfecd88002d
Giacomo Melacini <gmelacini@unibz.it>
2021-01-07 14:58:42 +0100

    Changed import position

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from move import bp
-from stats import stats_bp
 
 server = Flask(__name__)
 
 server.config.from_object(os.environ['APP_SETTINGS'])
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(server)
+
+from move import bp
+from stats import stats_bp
 
 server.register_blueprint(bp)
 server.register_blueprint(stats_bp)

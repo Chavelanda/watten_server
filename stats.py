3e3dc6525a98a469903e39edfa61c8dbb7499594
Giacomo Melacini <gmelacini@unibz.it>
2021-01-07 15:17:24 +0100

    Added route to get stats for each generation

--- stats.py
+++ stats.py
@@ -5,6 +5,21 @@ stats_bp = Blueprint("stats", __name__, url_prefix='/stats')
 
 from models import Stats
 
+
+@stats_bp.route('/get/<gen_>')
+def get_stats(gen_):
+
+    if -1 <= gen_ < 4:
+        try:
+            stat = Stats.query.filter_by(generation=gen_).first()
+            return jsonify(stat.serialize())
+        except Exception as e:
+            return str(e)
+
+    return jsonify({'error': 'The generation does not exists'}), 400
+
+
+
 # Initialization of rows in the table stats
 if __name__ == '__main__':
     db.session.add_all([
